5a1d99a7a3e99997498ec93ebadda73f80a8733a
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-01-30 11:42:40 -0500

    derp

--- Rotating Line Project/src/Frame.java
+++ Rotating Line Project/src/Frame.java
@@ -1,9 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+/**
+ * 
+ * @author Kevin Zhan
+ *
+ */
 
 public class Frame extends JFrame{
 		JPanel toolbar;
 		ScreenPanel screen;
-
+//okey dokey
 }
