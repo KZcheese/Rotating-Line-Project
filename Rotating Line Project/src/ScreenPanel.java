fc176c5e9e779f9594f17d0059aee8e845e8b197
Kevin Zhan <kevin.w.zhan@gmail.com>
2015-02-23 11:42:27 -0500

    YOOOOOOOOOOO

--- Rotating Line Project/src/ScreenPanel.java
+++ Rotating Line Project/src/ScreenPanel.java
@@ -4,9 +4,13 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Polygon;
-
 import javax.swing.JPanel;
-
+/** 
+ * A class that displays a regular polygon that can rotate with modifiable line width, side number, radius, and color.
+ * @author Kevin Zhan
+ * @author Umanga Balasuriya
+ * @version Last updated 2/23/15
+ */
 @SuppressWarnings("serial")
 public class ScreenPanel extends JPanel {
 	private int radius;
@@ -15,6 +19,13 @@ public class ScreenPanel extends JPanel {
 	private int sides;
 	private Color color;
 
+	/**
+	 * @param sides
+	 * @param radius
+	 * @param stroke
+	 * @param angle
+	 * @param color
+	 */
 	public ScreenPanel(int sides, int radius, int stroke, double angle,
 			Color color) {
 		this.radius = radius;
@@ -27,7 +38,10 @@ public class ScreenPanel extends JPanel {
 	public ScreenPanel() {
 		this(2, 100, 0, 0, new Color(0, 0, 0));
 	}
-
+/**
+ * Rotates the polygon by the given degrees.
+ * @param angle
+ */
 	public void rotate(double angle) {
 		this.angle += angle;
 		if (angle > 360)
